9269a964712b505e539915a805a52b4ee9476d81
Faimi the Legend Zufarov <faimi14102001@gmail.com>
2024-08-26 14:51:54 +0400

    Sorted Vacancies Screen implemented

--- screen/search/src/main/java/ru/faimizufarov/search/ui/SearchFragment.kt
+++ screen/search/src/main/java/ru/faimizufarov/search/ui/SearchFragment.kt
@@ -1,10 +1,12 @@
 package ru.faimizufarov.search.ui
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
@@ -12,8 +14,6 @@ import androidx.lifecycle.ViewModelProvider
 import ru.faimizufarov.domain.models.Offer
 import ru.faimizufarov.domain.models.Result
 import ru.faimizufarov.domain.models.Vacancy
-import ru.faimizufarov.domain.repository.ResultRepository
-import ru.faimizufarov.search.R
 import ru.faimizufarov.search.adapter.OfferAdapter
 import ru.faimizufarov.search.adapter.VacancyAdapter
 import ru.faimizufarov.search.databinding.FragmentSearchBinding
@@ -26,6 +26,9 @@ class SearchFragment : Fragment() {
     @Inject
     lateinit var searchViewModelFactory: SearchViewModelFactory
     private lateinit var searchViewModel: SearchViewModel
+
+    private val offerAdapter = OfferAdapter(onItemClick = ::updateOfferFeed)
+    private val vacancyAdapter = VacancyAdapter(onItemClick = ::updateVacancyFeed)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,19 +54,71 @@ class SearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val offerAdapter = OfferAdapter(onItemClick = ::updateOfferFeed)
-        val vacancyAdapter = VacancyAdapter(onItemClick = ::updateVacancyFeed)
-
         binding.offersRecyclerView.adapter = offerAdapter
         binding.vacanciesRecyclerView.adapter = vacancyAdapter
+        observeViewModel()
+    }
 
+    @SuppressLint("ClickableViewAccessibility")
+    private fun observeViewModel() {
         searchViewModel.result.observe(viewLifecycleOwner) { result ->
             offerAdapter.submitList(result.offers)
             vacancyAdapter.submitList(result.vacancies.take(3))
 
             binding.moreVacanciesButton.visibility = View.VISIBLE
             correctVacancyEnding(result)
+
+            binding.moreVacanciesButton.setOnClickListener {
+                vacancyAdapter.submitList(result.vacancies)
+                with(binding) {
+                    queryEditText.setCompoundDrawablesWithIntrinsicBounds(
+                        resources.getDrawable(ru.faimizufarov.core.R.drawable.back_arrow_icon),
+                        null,
+                        null,
+                        null
+                    )
+
+                    vacancyNumberTextView.visibility = View.VISIBLE
+                    sortTextView.visibility = View.VISIBLE
+                    sortImageView.visibility = View.VISIBLE
+                    it.visibility = View.GONE
+
+                    vacancyNumberTextView.text = resources.getQuantityString(
+                        ru.faimizufarov.core.R.plurals.only_vacancies_count,
+                        result.vacancies.size,
+                        result.vacancies.size
+                    )
+                }
+            }
+
+            binding.queryEditText.setOnTouchListener { _, event ->
+                if (event.action == MotionEvent.ACTION_UP) {
+                    val drawableBounds = binding.queryEditText.compoundDrawables[0]?.bounds
+                    val drawableWidth = drawableBounds?.width() ?: 0
+
+                    if (event.x <= (binding.queryEditText.paddingLeft + drawableWidth)) {
+                        onDrawableStartClick(result)
+                        return@setOnTouchListener true
+                    }
+                }
+                false
+            }
+        }
+    }
+
+    private fun onDrawableStartClick(result: Result) {
+        with(binding) {
+            sortImageView.visibility = View.GONE
+            sortTextView.visibility = View.GONE
+            vacancyNumberTextView.visibility = View.GONE
+            moreVacanciesButton.visibility = View.VISIBLE
+            queryEditText.setCompoundDrawablesWithIntrinsicBounds(
+                resources.getDrawable(ru.faimizufarov.core.R.drawable.action_search),
+                null,
+                null,
+                null
+            )
+            vacancyAdapter.submitList(result.vacancies.take(3))
         }
     }
 
@@ -79,7 +134,7 @@ class SearchFragment : Fragment() {
     }
 
     private fun correctVacancyEnding(result: Result) {
-        val lastVacanciesNumber = result.vacancies.size
+        val lastVacanciesNumber = result.vacancies.size - 3
         binding.moreVacanciesButton.text = resources.getQuantityString(
             ru.faimizufarov.core.R.plurals.vacancies_count,
             lastVacanciesNumber,

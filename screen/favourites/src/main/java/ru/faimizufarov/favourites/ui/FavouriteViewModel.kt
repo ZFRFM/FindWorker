61f19e2f929960b756d5e7d6b77b100f6502776c
Faimi the Legend Zufarov <faimi14102001@gmail.com>
2024-08-27 10:04:23 +0400

    FavouriteViewModel & FavouriteViewModelFactory implemented

--- screen/favourites/src/main/java/ru/faimizufarov/favourites/ui/FavouriteViewModel.kt
+++ screen/favourites/src/main/java/ru/faimizufarov/favourites/ui/FavouriteViewModel.kt
@@ -1,4 +1,24 @@
 package ru.faimizufarov.favourites.ui
 
-class FavouriteViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import ru.faimizufarov.domain.models.Vacancy
+import ru.faimizufarov.domain.usecase.GetFavouritesUseCase
+
+class FavouriteViewModel(
+    private val getFavouritesUseCase: GetFavouritesUseCase
+): ViewModel() {
+
+    private val _favouriteVacanciesLiveData = MutableLiveData<List<Vacancy>>()
+    val favouriteVacanciesLiveData: LiveData<List<Vacancy>> = _favouriteVacanciesLiveData
+
+    init {
+        viewModelScope.launch {
+            val favouriteVacancies = getFavouritesUseCase.execute()
+            _favouriteVacanciesLiveData.value = favouriteVacancies
+        }
+    }
 }
--- screen/favourites/src/main/java/ru/faimizufarov/favourites/ui/FavouriteViewModelFactory.kt
+++ screen/favourites/src/main/java/ru/faimizufarov/favourites/ui/FavouriteViewModelFactory.kt
@@ -1,4 +1,17 @@
 package ru.faimizufarov.favourites.ui
 
-class FavouriteViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import ru.faimizufarov.domain.usecase.GetFavouritesUseCase
+import javax.inject.Inject
+
+class FavouriteViewModelFactory
+@Inject
+constructor(
+    val getFavouritesUseCase: GetFavouritesUseCase
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T =
+        FavouriteViewModel(
+            getFavouritesUseCase
+        ) as T
 }
